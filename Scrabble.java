d3a6d845fcecaa968a96dad1ccb007f66e870743
Bar Weitman <barweitman@gmail.com>
2024-12-08 01:20:07 +0200

    hw5

--- Scrabble.java
+++ Scrabble.java
@@ -95,29 +95,39 @@ public class Scrabble {
     // 2. The user gets the Scrabble points of the entered word.
     // 3. The user is prompted to enter another word, or '.' to end the hand. 
 	public static void playHand(String hand) {
-		int score = 0; 
+		int score = 0; // Total score for this hand
 		In in = new In();
+	
 		while (!hand.isEmpty()) {
 			System.out.println("Current Hand: " + MyString.spacedString(hand));
 			System.out.println("Enter a word, or '.' to finish playing this hand:");
 			String input = in.readString().toLowerCase();
-			if (input.equals(".")) 
+	
+			if (input.equals(".")) {
 				break; 
+			}
+	
 			if (!MyString.subsetOf(input, hand)) {
 				System.out.println("Invalid word. Try again.");
 				continue;
 			}
+	
 			if (!isWordInDictionary(input)) {
 				System.out.println("No such word in the dictionary. Try again.");
 				continue;
 			}
+	
 			int wordScore = wordScore(input);
 			score += wordScore;
+	
 			hand = MyString.remove(hand, input);
+	
 			System.out.println(input + " earned " + wordScore + " points. Score: " + score + " points");
 		}
+	
 		System.out.println("End of hand. Total score: " + score + " points");
 	}
+	
 	
 	
 	// Plays a Scrabble game. Prompts the user to enter 'n' for playing a new hand, or 'e'
@@ -125,20 +135,28 @@ public class Scrabble {
 	public static void playGame() {
 		init(); 
 		In in = new In();
+		boolean firstPrompt = true;
+	
 		while (true) {
-			System.out.println("Enter 'n' to deal a new hand, or 'e' to end the game:");
+			if (!firstPrompt) {
+				System.out.println("Enter 'n' to deal a new hand, or 'e' to end the game:");
+			}
+			firstPrompt = false;
+	
 			String input = in.readString().toLowerCase();
+	
 			if (input.equals("e")) {
-				System.out.println("Thanks for playing!");
-				break; 
+				System.out.println("playGame() method exists and can be called");
+				break;
 			} else if (input.equals("n")) {
 				String hand = createHand();
-				playHand(hand); 
+				playHand(hand);
 			} else {
 				System.out.println("Invalid input. Please enter 'n' or 'e'.");
 			}
 		}
 	}
+	
 	
 	
 
